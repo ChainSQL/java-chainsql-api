bf59226f96a40b958e1e4dcb48c1385f05692223
wangxp <wangxp_2014@163.com>
2017-04-11 16:24:28 +0800

    修改字符编码

--- src/main/java/com/peersafe/chainsql/util/JSONUtil.java
+++ src/main/java/com/peersafe/chainsql/util/JSONUtil.java
@@ -1,5 +1,6 @@
 package com.peersafe.chainsql.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
@@ -92,20 +93,31 @@ public class JSONUtil {
      * @throws IOException
      */
 	public static String toHexString(String s){
-		String strr = "";
-		try {
-			strr = new String(s.getBytes("gb2312"),"utf-8");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		System.out.println(strr);
-		String str="";
-		for (int i=0;i<s.length();i++)
-		{
-		int ch = (int)s.charAt(i);
-		String s4 = Integer.toHexString(ch);
-		str = str + s4;
-		}
+		String str = encode(s);
 		return str;
+	} 
+	private static String hexString = "0123456789ABCDEF";
+	 
+	public static String encode(String str){
+		//根据默认编码获取字节数组
+	byte[] bytes=str.getBytes();
+	StringBuilder sb=new StringBuilder(bytes.length*2);
+	//将字节数组中每个字节拆解成2位16进制整数
+	    for(int i=0;i<bytes.length;i++)
+	    {
+	    sb.append(hexString.charAt((bytes[i]&0xf0)>>4));
+	    sb.append(hexString.charAt((bytes[i]&0x0f)>>0));
+	    }
+	    return sb.toString();
+	}
+	/*
+	* 将16进制数字解码成字符串,适用于所有字符（包括中文）
+	*/
+	public static String decode(String bytes){
+	    ByteArrayOutputStream baos=new ByteArrayOutputStream(bytes.length()/2);
+	    //将每2位16进制整数组装成一个字节
+	    for(int i=0;i<bytes.length();i+=2)
+	    baos.write((hexString.indexOf(bytes.charAt(i))<<4 |hexString.indexOf(bytes.charAt(i+1))));
+	    return new String(baos.toByteArray());
 	} 
 }

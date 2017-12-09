f249df1564697b300d87816b0d8dc40aca13e768
luleigreat <luleigreat@163.com>
2017-12-09 17:25:18 +0800

    change address

--- src/test/java/java8/test/TestSign.java
+++ src/test/java/java8/test/TestSign.java
@@ -36,26 +36,26 @@ public class TestSign {
 		
 		JSONObject limitAmount = new JSONObject();
 		limitAmount.put("currency", "GRD");
-		limitAmount.put("issuer", "r3kmLJN5D28dHuH8vZNUZpMC43pEHpaocV");
+		limitAmount.put("issuer", "zhWACtoujvNmmER29zVXrCxxFzpitPajdm");
 		limitAmount.put("value", "1000");
 		
 		JSONObject tx_json = new JSONObject();
-		tx_json.put("Account", "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh");
+		tx_json.put("Account", "zHb9CJAWyB4zj91VRWn96DkukG4bwdtyTh");
 		tx_json.put("LimitAmount", limitAmount);
 		tx_json.put("Flags", 0);
 		tx_json.put("TransactionType", "TrustSet");
 		tx_json.put("Sequence", 2);
 		obj.put("tx_json", tx_json);
 		
-		JSONObject res = c.sign(obj, "snoPBrXtMeMyMHUVTgbuqAfg1SUTb");
+		JSONObject res = c.sign(obj, "xnoPBzXtMeMyMHUVTgbuqAfg1SUTb");
 		System.out.println("sign TrustSet result:" + res);
 	}
 
 	private static void testSignAccountSet(){
 		JSONObject obj = new JSONObject();
-		obj.put("secret", "snoPBrXtMeMyMHUVTgbuqAfg1SUTb");
+		obj.put("secret", "xnoPBzXtMeMyMHUVTgbuqAfg1SUTb");
 		JSONObject tx_json = new JSONObject();
-		tx_json.put("Account", "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh");
+		tx_json.put("Account", "zHb9CJAWyB4zj91VRWn96DkukG4bwdtyTh");
 		tx_json.put("SetFlag", 8);
 		tx_json.put("TransactionType", "AccountSet");
 		JSONObject memo = new JSONObject();
@@ -68,33 +68,33 @@ public class TestSign {
 		tx_json.put("Sequence", 2);
 		obj.put("tx_json", tx_json);
 		
-		JSONObject res = c.sign(obj, "snoPBrXtMeMyMHUVTgbuqAfg1SUTb");
+		JSONObject res = c.sign(obj, "xnoPBzXtMeMyMHUVTgbuqAfg1SUTb");
 		System.out.println("sign AccountSet result:" + res);
 	}
 
 	private static void testSignSignerListSet(){
 		JSONObject obj = new JSONObject();
-		obj.put("secret", "snoPBrXtMeMyMHUVTgbuqAfg1SUTb");
+		obj.put("secret", "xnoPBzXtMeMyMHUVTgbuqAfg1SUTb");
 		JSONObject tx_json = new JSONObject();
-		tx_json.put("Account", "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh");
+		tx_json.put("Account", "zHb9CJAWyB4zj91VRWn96DkukG4bwdtyTh");
 		tx_json.put("TransactionType", "SignerListSet");
 		tx_json.put("SignerQuorum", 3);
 		
 		JSONObject entry1 = new JSONObject();
-		entry1.put("Account", "rDsFXt1KRDNNckSh3exyTqkQeBKQCXawb2");
+		entry1.put("Account", "zMcXHEkD78T1pwAgG2pf6QWALyBKF1YvD1");
 		entry1.put("SignerWeight", 2);
 		JSONObject entry11 = new JSONObject();
 		entry11.put("SignerEntry", entry1);
 		
 		JSONObject entry2 = new JSONObject();
-		entry2.put("Account", "rPeRx9WUAivWPpqJnT1ZkDV5r845Rui1Mp");
+		entry2.put("Account", "zhWACtoujvNmmER29zVXrCxxFzpitPajdm");
 		entry2.put("SignerWeight", 1);
 		JSONObject entry22 = new JSONObject();
 		entry22.put("SignerEntry", entry2);
 		
 		
 		JSONObject entry3 = new JSONObject();
-		entry3.put("Account", "rL9UctLXeQmvdX6T7JR4yNz2dmgtN8awzq");
+		entry3.put("Account", "zKvHeBUtEoNRW1wtvA42tfJx1bh7pqxZmT");
 		entry3.put("SignerWeight", 1);
 		JSONObject entry33 = new JSONObject();
 		entry33.put("SignerEntry", entry3);
@@ -109,45 +109,45 @@ public class TestSign {
 		tx_json.put("Sequence", 2);
 		obj.put("tx_json", tx_json);
 		
-		JSONObject res = c.sign(obj, "snoPBrXtMeMyMHUVTgbuqAfg1SUTb");
+		JSONObject res = c.sign(obj, "xnoPBzXtMeMyMHUVTgbuqAfg1SUTb");
 		System.out.println("sign signerListSet result:" + res);
 	}
 	
 	private static void testSignForPayment(){
 		JSONObject obj = new JSONObject();
-		obj.put("secret", "snoPBrXtMeMyMHUVTgbuqAfg1SUTb");
-		obj.put("account","rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh");
+		obj.put("secret", "xnoPBzXtMeMyMHUVTgbuqAfg1SUTb");
+		obj.put("account","zHb9CJAWyB4zj91VRWn96DkukG4bwdtyTh");
 		JSONObject tx_json = new JSONObject();
-		tx_json.put("Account", "rBuLBiHmssAMHWQMnEN7nXQXaVj7vhAv6Q");
+		tx_json.put("Account", "zKvHeBUtEoNRW1wtvA42tfJx1bh7pqxZmT");
 		tx_json.put("Amount", "1000000000");
-		tx_json.put("Destination", "rfnmWkQ1jpVvWFvw7rnFNt7AUe58rQvMet");
+		tx_json.put("Destination", "zMcXHEkD78T1pwAgG2pf6QWALyBKF1YvD1");
 		tx_json.put("TransactionType", "Payment");
 		tx_json.put("Sequence", 2);
 		obj.put("tx_json", tx_json);
 		
-		JSONObject res = c.signFor(obj, "snoPBrXtMeMyMHUVTgbuqAfg1SUTb");
+		JSONObject res = c.signFor(obj, "xnoPBzXtMeMyMHUVTgbuqAfg1SUTb");
 		System.out.println("sign_for payment signer:" + res);
 	}
 	
 	private static void testSignForTrustSet(){
 		JSONObject obj = new JSONObject();
-		obj.put("secret", "snoPBrXtMeMyMHUVTgbuqAfg1SUTb");
-		obj.put("account","rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh");
+		obj.put("secret", "xnoPBzXtMeMyMHUVTgbuqAfg1SUTb");
+		obj.put("account","zHb9CJAWyB4zj91VRWn96DkukG4bwdtyTh");
 		
 		JSONObject limitAmount = new JSONObject();
 		limitAmount.put("currency", "GRD");
-		limitAmount.put("issuer", "rBuLBiHmssAMHWQMnEN7nXQXaVj7vhAv6Q");
+		limitAmount.put("issuer", "zMcXHEkD78T1pwAgG2pf6QWALyBKF1YvD1");
 		limitAmount.put("value", "1000");
 		
 		JSONObject tx_json = new JSONObject();
-		tx_json.put("Account", "rfnmWkQ1jpVvWFvw7rnFNt7AUe58rQvMet");
+		tx_json.put("Account", "zBB1AiU6kGCBiUL2LPbimNp15cBkJLV7bp");
 		tx_json.put("LimitAmount", limitAmount);
 		tx_json.put("Flags", 0);
 		tx_json.put("TransactionType", "TrustSet");
 		tx_json.put("Sequence", 2);
 		obj.put("tx_json", tx_json);
 		
-		JSONObject res = c.signFor(obj, "snoPBrXtMeMyMHUVTgbuqAfg1SUTb");
+		JSONObject res = c.signFor(obj, "xnoPBzXtMeMyMHUVTgbuqAfg1SUTb");
 		System.out.println("sign_for trustset signer:" + res);
 	}
 	
@@ -155,14 +155,14 @@ public class TestSign {
 		JSONObject ptPayment = new JSONObject();
 	    ptPayment.put("Amount", "10000000000");
 	    ptPayment.put("Fee", "10000");
-	    ptPayment.put("Destination", "r3u4v4xqyweCeS2DYoFvWGmAJo2tphmUfc");
-	    ptPayment.put("Account", "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh");
+	    ptPayment.put("Destination", "zMcXHEkD78T1pwAgG2pf6QWALyBKF1YvD1");
+	    ptPayment.put("Account", "zHb9CJAWyB4zj91VRWn96DkukG4bwdtyTh");
 	    ptPayment.put("Sequence", 1);
 	    ptPayment.put("TransactionType", "Payment");
 
 	    JSONObject sendMax = new JSONObject();
 	    sendMax.put("currency", "GRD");
-	    sendMax.put("issuer", "r3u4v4xqyweCeS2DYoFvWGmAJo2tphmUfc");
+	    sendMax.put("issuer", "zKvHeBUtEoNRW1wtvA42tfJx1bh7pqxZmT");
 	    sendMax.put("value", "10000");
 	    ptPayment.put("SendMax", sendMax);
 
@@ -170,7 +170,7 @@ public class TestSign {
 	    pathObj.put("type_hex", "0000000000000030");
 	    pathObj.put("currency", "GRD");
 	    pathObj.put("type", 48);
-	    pathObj.put("issuer", "r3u4v4xqyweCeS2DYoFvWGmAJo2tphmUfc");
+	    pathObj.put("issuer", "zBB1AiU6kGCBiUL2LPbimNp15cBkJLV7bp");
 	    //ptPayment.push_back(std::make_pair("SendMax", sendMax));
 
 	    JSONArray   arrayPathInner  = new JSONArray();
@@ -183,7 +183,7 @@ public class TestSign {
 	    JSONObject obj = new JSONObject();
 	    obj.put("tx_json", ptPayment);
 //	    System.out.println(ptPayment);
-	    JSONObject res = c.sign(obj, "sn9Bz7hSxpzzBapdxvf9dP7k8rTfu");
+	    JSONObject res = c.sign(obj, "xn6ph3eheqhv12SrcbEPt8jsAG5M6");
 		System.out.println("sign for Pathset signer:" + res);
 	}
 }

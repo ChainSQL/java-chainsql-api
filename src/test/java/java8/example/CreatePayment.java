b59dadc239ae4991d57478c493c2c45a383caa24
luleigreat <luleigreat@163.com>
2018-07-31 17:31:46 +0800

    payment address change

--- src/test/java/java8/example/CreatePayment.java
+++ src/test/java/java8/example/CreatePayment.java
@@ -2,6 +2,8 @@ package java8.example;
 
 import static java8.example.Print.*;
 
+import java.math.BigDecimal;
+
 import com.peersafe.base.client.Account;
 import com.peersafe.base.client.Client;
 import com.peersafe.base.client.responses.Response;
@@ -10,6 +12,7 @@ import com.peersafe.base.client.transactions.TransactionManager;
 import com.peersafe.base.client.transport.impl.JavaWebSocketTransportImpl;
 import com.peersafe.base.core.coretypes.AccountID;
 import com.peersafe.base.core.coretypes.Amount;
+import com.peersafe.base.core.coretypes.Currency;
 import com.peersafe.base.core.coretypes.uint.UInt32;
 import com.peersafe.base.core.types.known.tx.result.TransactionResult;
 import com.peersafe.base.core.types.known.tx.txns.Payment;
@@ -20,8 +23,8 @@ public class CreatePayment {
 	        // We need a valid seed
 
 	           new Client(new JavaWebSocketTransportImpl())
-	                    .connect("ws://192.168.0.110:6007", (c) ->
-	                        new CreatePayment(c, "snoPBrXtMeMyMHUVTgbuqAfg1SUTb"));
+	                    .connect("ws://192.168.0.90:6006", (c) ->
+	                        new CreatePayment(c, "xpiRhPbd98y3UiPyyEUNK2N3Kird3"));
 	 
 	    }
 
@@ -31,11 +34,14 @@ public class CreatePayment {
 
 	        Payment payment = new Payment();
 
-	        payment.as(AccountID.Account,     "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh")
-	             	.as(AccountID.Destination, "rKuHVwdAsL4Ldm2CftWZGNR5pLVbBFDuwk")
-	             	.as(Amount.Fee,            "10")
-	             	.as(UInt32.Sequence,        1)
-	             	.as(Amount.Amount, "100000000");
+	        Amount amount = new Amount(BigDecimal.valueOf(100),Currency.fromString("GRD123456"),
+	        		AccountID.fromAddress("znbWk4iuz2HL1e1Ux91TzYfFzJHGeYxBA4"));
+	        Amount max = new Amount(BigDecimal.valueOf(102),Currency.fromString("GRD123456"),
+	        		AccountID.fromAddress("znbWk4iuz2HL1e1Ux91TzYfFzJHGeYxBA4"));
+	        payment.as(AccountID.Account,     "zHYfrrZyyfAMrNgm3akQot6CuSmMM6MLda")
+	             	.as(AccountID.Destination, "zMbBhnQAPu7KHgtRXWFxp4YGX4LjtMpgo1")
+	             	.as(Amount.Amount, amount)
+	             	.as(Amount.SendMax, max);
 
 	        tm.queue(tm.manage(payment)
 	            .onValidated(this::onValidated)
@@ -51,6 +57,7 @@ public class CreatePayment {
 	    }
 
 	    private void onError(Response res) {
+	    	System.out.println(res.message);
 	        printErr("Transaction failed!");
 //	        managed.submissions.forEach(sub ->
 //	                printErr("{0}", sub.hash) );

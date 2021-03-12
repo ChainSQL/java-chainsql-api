7e0cbc390db725a2ba2ede3290b0fa97f0c8ab37
luleigreat <luleigreat@163.com>
2021-03-12 11:24:33 +0800

    split schema related example to TestSchema.java

--- chainsql/src/test/java/com/peersafe/example/chainsql/TestChainsql.java
+++ chainsql/src/test/java/com/peersafe/example/chainsql/TestChainsql.java
@@ -3,17 +3,12 @@ package com.peersafe.example.chainsql;
 import java.io.*;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.peersafe.base.client.pubsub.Publisher.Callback;
 import com.peersafe.chainsql.core.Chainsql;
 import com.peersafe.chainsql.core.Ripple;
-import com.peersafe.chainsql.core.Submit.SchemaOpType;
 import com.peersafe.chainsql.core.Submit.SyncCond;
 import com.peersafe.chainsql.util.Util;
 
@@ -114,21 +109,6 @@ public class TestChainsql {
 		test.testModifyTable();
 	}
 	
-	private static void testSchema() {
-		TestChainsql test = new TestChainsql();
-		c.as(rootAddress, rootSecret);
-		
-//		test.testSchemaList();
-		
-//		test.testSchemaCreate();
-		
-//		sTableName = "hello_123";
-//		c.setSchema("57256592FD987D4256DDCE4812484BDE9B9193A70DDEC983541A182275045FE0");
-//		test.testCreateTable();
-		
-		test.testSchemaModify();
-	}
-	
 	
 	private static void testRipple() {
 		TestChainsql test = new TestChainsql();
@@ -168,69 +148,6 @@ public class TestChainsql {
 //			}
 //			
 //		});
-	}
-	
-	public void testSchemaList() {
-		JSONObject option = new JSONObject();
-		JSONObject list = c.getSchemaList(option);
-		System.out.println(list);
-	}
-	
-	public void testSchemaCreate() {
-		JSONObject schemaInfo = new JSONObject();
-		schemaInfo.put("SchemaName","hello1");
-		schemaInfo.put("WithState",false);
-
-		schemaInfo.put("SchemaAdmin",rootAddress);
-
-		List<String> validators = new ArrayList<String>();
-		validators.add("03C53D4B7E4D558DBD8EA67E68AD97844FCCF48DD4C7A5C10E05B293A11DC9BB40");
-		validators.add("021D3E9C571DF23054DBB2005E76EA5BE5227D381FB9B4A52467B5E6412ABAFBA0");
-		validators.add("0317B5CAEBE6C778D133B1CA670D00E994D3AFAC2C0E6AA8F11B0DA277309F193E");
-		JSONArray validatorsJsonArray = new JSONArray(validators);
-		schemaInfo.put("Validators",validatorsJsonArray);
-
-		List<String> peerList = new ArrayList<String>();
-		peerList.add("127.0.0.1:5125");
-		peerList.add("127.0.0.1:5126");
-		peerList.add("127.0.0.1:5127");
-		JSONArray peerListJsonArray = new JSONArray(peerList);
-
-		schemaInfo.put("PeerList",peerListJsonArray);
-		schemaInfo.put("SchemaAdmin",rootAddress);
-		
-		try {
-			JSONObject ret = c.createSchema(schemaInfo).submit(SyncCond.validate_success);
-			System.out.println(ret);
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public void testSchemaModify() {
-		JSONObject schemaInfo = new JSONObject();
-		schemaInfo.put("SchemaID", "312242E6D2DB07BF1F856A2525A2DBA4199F9B60CF38584C5815CD79808572FD");
-		
-		List<String> validators = new ArrayList<String>();
-		validators.add("02DB6543999A94815F7FC5F61D3AB1BA60938F15BC004E70613F7824DA70D5CCAB");
-		JSONArray validatorsJsonArray = new JSONArray(validators);
-		schemaInfo.put("Validators",validatorsJsonArray);
-
-		List<String> peerList = new ArrayList<String>();
-		peerList.add("127.0.0.1:5128");
-		JSONArray peerListJsonArray = new JSONArray(peerList);
-
-		schemaInfo.put("PeerList",peerListJsonArray);
-		
-		try {
-			JSONObject obj = c.modifySchema(SchemaOpType.schema_add, schemaInfo).submit(SyncCond.validate_success);
-			System.out.println(obj);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 	
 	public void generateAccount() {
